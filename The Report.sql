ae2f1bce4f28881822e9021898b6d219206fa6a3
Prajyot-21 <prajyot.gurav21@gmail.com>
2024-12-23 12:36:38 +0530

    Add files via upload

---
+++ The Report.sql
@@ -0,0 +1,6 @@
+with g as (
+select id,name, marks ,grade from students,grades
+where marks >=min_mark and marks <= max_mark )
+select case when grade > 7 Then name else null end as name , grade ,marks from g 
+order by grade desc,
+case when grade > 7 then name else marks end 
